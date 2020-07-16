1d586bcd8276a625d11bb2370da922b1d9eeca8d
attsu1003 <attsuyagimpul@gmail.com>
2020-07-17 01:19:40 +0900

    空き状況照会機能新規作成

--- src/main/java/com/example/demo/common/DateUtil.java
+++ src/main/java/com/example/demo/common/DateUtil.java
@@ -1,21 +1,11 @@
 package com.example.demo.common;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class DateUtil {
-	private static Calendar calendar = Calendar.getInstance();
-
-	public static Date date(String day) {
-		try {
-			return new SimpleDateFormat("yyyy-MM-dd").parse(day);
-		} catch (ParseException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
 
 	public static long diffDate(Date before, Date after) {
 		final long oneDay = 24 * 60 * 60 * 1000;
@@ -24,46 +14,22 @@ public class DateUtil {
 	}
 
 	public static synchronized Date getNextDate(Date date) {
+		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(date);
 		calendar.add(Calendar.DAY_OF_MONTH, 1);
 		return calendar.getTime();
 	}
 
-	public static synchronized Date getAddMonth(Date date, int amount) {
-		calendar.setTime(date);
-		calendar.add(Calendar.DATE, amount);
-		return calendar.getTime();
-	}
-
-	public static synchronized Date getDate(int year, int month, int day) {
-		
-		calendar.set(year, month - 1, day);
-		return calendar.getTime();
-	}
-	
-	public static synchronized Date getDate2(String year, String month, String day) {
+	public static synchronized Date getDateFrom(String year, String month, String day) {
+		Calendar calendar = Calendar.getInstance();
 		calendar.set(Integer.parseInt(year), Integer.parseInt(year), Integer.parseInt(day));
 		return calendar.getTime();
-		
+
 	}
 
 	public static int countMonthDays(Date date) {
 		Calendar c = new GregorianCalendar(Integer.parseInt(new SimpleDateFormat("yyyy").format(date)),
-				Integer.parseInt(new SimpleDateFormat("MM").format(date)), 1);
-
+				Integer.parseInt(new SimpleDateFormat("MM").format(date)) - 1, 1);
 		return c.getActualMaximum(Calendar.DAY_OF_MONTH);
-	}
-
-	public static Date firstDate(Date date) {
-		return new Date(Integer.parseInt(new SimpleDateFormat("yyyy").format(date)),
-				Integer.parseInt(new SimpleDateFormat("MM").format(date)), 1);
-	}
-
-	public static Date lastDate(Date date) {
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(date);
-		return new Date(Integer.parseInt(new SimpleDateFormat("yyyy").format(date)),
-				Integer.parseInt(new SimpleDateFormat("MM").format(date)), calendar.getActualMaximum(Calendar.DATE));
-
 	}
 }
--- src/main/java/com/example/demo/controller/ReserveReferVacancyController.java
+++ src/main/java/com/example/demo/controller/ReserveReferVacancyController.java
@@ -10,17 +10,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.demo.application.query.ReserveReferApplicationQueryServiceImpl;
 import com.example.demo.common.DateUtil;
 import com.example.demo.domain.reserve.ReserveRepository;
 import com.example.demo.domain.room.RoomRepository;
 
 @Controller
 public class ReserveReferVacancyController {
-
-//	@Autowired
-//	private ReserveReferApplicationQueryServiceImpl reserveReferApplicationQueryServiceImpl;
 
 	@Autowired
 	private ReserveRepository reserverepository;
@@ -32,49 +29,48 @@ public class ReserveReferVacancyController {
 	public String referVacancy(Model model) {
 		model.addAttribute("message", "空き状況照会画面");
 
-		int roomNum = roomRepository.countRoom();
-		List<String> roomStatements = new ArrayList<>();
-
 		Date date = new Date();
-		int monthDays = DateUtil.countMonthDays(date);
-		for (int i = 1; i <= monthDays; i++) {
-			// int reserveCount = reserverepository.countReserve(DateUtil.firstDate(date));
-			int reserveCount = reserverepository
-					.countReserve(DateUtil.getDate2(new SimpleDateFormat("yyyy").format(date),
-							new SimpleDateFormat("MM").format(date), String.format("%02d", i)));
 
-			roomStatements.add(this.roomStatement(reserveCount, roomNum));
-			date = DateUtil.getNextDate(date);
-		}
-		model.addAttribute("roomStatements", roomStatements);
+		SimpleDateFormat ysdf = new SimpleDateFormat("yyyy");
+		int year = Integer.parseInt(ysdf.format(date));
+		model.addAttribute("year", year);
+		SimpleDateFormat msdf = new SimpleDateFormat("MM");
+		int month = Integer.parseInt(msdf.format(date));
+		model.addAttribute("month", month);
+		model.addAttribute("roomStatements",
+				this.checkOneMonthRoomStatements(Integer.parseInt(new SimpleDateFormat("yyyy").format(date)),
+						Integer.parseInt(new SimpleDateFormat("MM").format(date))));
 		return "referVacancy";
 	}
-	
-	@RequestMapping(value = "/referVacancyDate", method = RequestMethod.POST)
-	public String referVacancyDate(Model model) {
+
+	@RequestMapping(value = "/referVacancyNextDate", method = RequestMethod.GET)
+	public String referVacancyNextDate(@RequestParam(name = "year") String year,
+			@RequestParam(name = "month") String month, Model model) {
 		model.addAttribute("message", "空き状況照会画面");
 
-		int roomNum = roomRepository.countRoom();
-		List<String> roomStatements = new ArrayList<>();
-
-		Date date = new Date();
-		int monthDays = DateUtil.countMonthDays(date);
-		for (int i = 1; i <= monthDays; i++) {
-			// int reserveCount = reserverepository.countReserve(DateUtil.firstDate(date));
-			int reserveCount = reserverepository
-					.countReserve(DateUtil.getDate2(new SimpleDateFormat("yyyy").format(date),
-							new SimpleDateFormat("MM").format(date), String.format("%02d", i)));
-
-			roomStatements.add(this.roomStatement(reserveCount, roomNum));
-			date = DateUtil.getNextDate(date);
-		}
-		model.addAttribute("roomStatements", roomStatements);
-		model.addAttribute("hoge", new hoge());
+		model.addAttribute("year", Integer.parseInt(year));
+		model.addAttribute("month", Integer.parseInt(month));
+		model.addAttribute("roomStatements",
+				this.checkOneMonthRoomStatements(Integer.parseInt(year), Integer.parseInt(month)));
 		return "referVacancy";
 	}
 
 	public int countReserve(Date date) {
 		return reserverepository.countReserve(date);
+	}
+
+	private List<String> checkOneMonthRoomStatements(int year, int month) {
+		List<String> roomStatements = new ArrayList<>();
+		Date date = new Date(year, month - 1, 1);
+		int roomNum = roomRepository.countRoom();
+		for (int i = 1; i <= DateUtil.countMonthDays(date); i++) {
+			int reserveCount = reserverepository
+					.countReserve(DateUtil.getDateFrom(new SimpleDateFormat("yyyy").format(date),
+							new SimpleDateFormat("MM").format(date), String.format("%02d", i)));
+			roomStatements.add(this.roomStatement(reserveCount, roomNum));
+			date = DateUtil.getNextDate(date);
+		}
+		return roomStatements;
 	}
 
 	String roomStatement(int reserveCount, int roomNum) {
@@ -87,11 +83,5 @@ public class ReserveReferVacancyController {
 			st = "〇";
 		}
 		return st;
-
 	}
-	
-	public static class hoge{
-		public int publicMethod() {return 2;}
-	}
-
 }
